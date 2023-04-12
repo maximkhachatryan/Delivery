d308f16cf536b6fa95b7383b827892a1539d2962
Maxim <maxim.khachatryan.2013@gmail.com>
2023-04-12 21:05:59 +0400

    Navigation

---
+++ components/Home/Application/Application_Step1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Application_Step1 = () =>{
+    return (
+        <Box></Box>
+    );
+}
+
+export default Application_Step1;
---
+++ components/Home/HomePage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+  Button
+} from "native-base";
+
+const HomePage = ({ navigation }) => {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Button onPress={() => navigation.navigate('NativeBase')}>Նոր առաքման հայտ</Button>
+        <Button onPress={() => navigation.navigate('NativeBase')}>Բոլոր առաքման պատվերները</Button>
+      </VStack>
+    </Center>
+  );
+}
+
+
+export default HomePage;
