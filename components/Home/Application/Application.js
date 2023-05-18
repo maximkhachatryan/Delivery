e6c002a4d1a35df38765e5cff256ad1fe646d937
Maxim <maxim.khachatryan.2013@gmail.com>
2023-05-19 00:39:13 +0400

    Changes

--- components/Home/Application/Application.js
+++ components/Home/Application/Application.js
@@ -1,54 +1,121 @@
 import React from "react";
-import { Text, HStack, Stack, Switch, FormControl, Box, Input, Select, CheckIcon, Radio, Heading, } from "native-base";
+import { Button, Stack, FormControl, Box, Input, Select, Radio, Heading, Checkbox, Divider, ScrollView, Container, TextArea } from "native-base";
 import { TimePickerInput } from "../../TimePicker";
+import { getVenderAddresses } from "../../../services/venderAddressesService";
+import CalendarInput from "../../CalendarInput";
 
 
-const Application = () => {
+const Application = ({ route, navigation }) => {
+    const { venderId } = route.params;
+
+    const [productDescription, setProductDescription] = React.useState("");
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
     const [pickupAddress, setPickupAddress] = React.useState("1");
     const [paymentBy, setPaymentBy] = React.useState("1");
+    const [selectedAddress, setSelectedAddress] = React.useState();
+    
+    const [addresses, setAddresses] = React.useState([]);
+    const [customerShouldPayForProduct, setCustomerShouldPayForProduct] = React.useState(false);
+
+
+
+
+    React.useEffect(() => {
+        //navigation.addListener('focus', async () => {
+        async function fetchData() {
+            let fetchedAddresses = await getVenderAddresses(venderId);
+            setAddresses(fetchedAddresses);
+            if (fetchedAddresses != null && fetchedAddresses.length != 0) {
+                setSelectedAddress(fetchedAddresses[0].id);
+            }
+        }
+        fetchData();
+        //});
+    }, []);
+
+    const getDistrictName = districtCode => {
+        switch (districtCode) {
+            case 1:
+                return 'Կենտրոն';
+            case 2:
+                return 'Արաբկիր';
+            case 3:
+                return 'Քանաքեռ Զեյթուն';
+            case 4:
+                return 'Ավան';
+            case 5:
+                return 'Նոր Նորք';
+            case 6:
+                return 'Նորք Մարաշ';
+            case 7:
+                return 'Էրեբունի';
+            case 8:
+                return 'Շենգավիթ';
+            case 9:
+                return 'Նուբարաշեն';
+            case 10:
+                return 'Մալաթիա-Սեբաստիա';
+            case 11:
+                return 'Աջափնյակ';
+            case 12:
+                return 'Դավթաշեն';
+            default:
+                return '...'
+        }
+    }
+
+    const addressItems = addresses.map((item, index) => {
+        return (
+            <Select.Item label={"ք․ Երևան, " + getDistrictName(item.district) + ", " + item.addressInfo} value={item.id} key={index} />
+        )
+    });
+
+    const handleAddressChange = (value) => {
+        setSelectedAddress(value);
+    };
+
+    const handleCustomerShouldPayForProduct = (newValue) => {
+        setCustomerShouldPayForProduct(newValue);
+    };
+
+
+
+    const minDate = new Date().toISOString().split('T')[0]; // set minimum date as today's date
+    const handleDateChange = (selectedDate) => {
+        console.log(`Selected date: ${selectedDate}`);
+    };
+
+    const onSubmit = () => {
+
+        navigation.goBack();
+    }
 
 
     return (
-        <Box alignItems="center" safeArea>
-            <Heading my="30px">Նոր առաքման հայտ</Heading>
-            <Box w="90%">
-                <FormControl isRequired>
+        <Box safeArea>
+            <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}>
+                <Heading my="30px">Նոր առաքման պատվեր</Heading>
+                <Box w="90%">
                     <Stack mx="0">
                         <FormControl.Label>Ապրանքի նկարագիր</FormControl.Label>
                         <Input type="Text" defaultValue="" placeholder="" />
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
                     </Stack>
                     <Stack mx="0">
                         <FormControl.Label>Ապրանքի գտնվելու վայրը</FormControl.Label>
-                        <Select accessibilityLabel="Choose Address" placeholder="" _selectedItem={{
-                            bg: "teal.600",
-                            endIcon: <CheckIcon size={5} />
-                        }} mt="1" isRequired>
-                            <Select.Item label="Երևան, Լյուքսեմբուրգի 7շ, 16 բն" value="1" />
-                            <Select.Item label="Երևան, Մառի 10շ, 43 բն" value="2" />
+                        <Select
+                            accessibilityLabel="Choose Address"
+                            placeholder=""
+                            mt="1"
+                            isRequired
+                            selectedValue={selectedAddress}
+                            onValueChange={handleAddressChange}
+                        >
+                            {addressItems}
                         </Select>
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
-
                     </Stack>
                     <Stack mx="0">
-                        <FormControl.Label>Վերցնելու ժամը (հնարավորինս շուտ)</FormControl.Label>
-                        <TimePickerInput />
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
+                        <FormControl.Label>Վերցնելու օրը</FormControl.Label>
+                        <CalendarInput onDateChange={handleDateChange} minDate={minDate} placeholder={"Ընտրեք ձեզ հարմար օրը"} />
                     </Stack>
                     <Stack mx="0">
                         <FormControl.Label>Առաքման գումարը կվճարվի</FormControl.Label>
@@ -59,53 +126,48 @@ const Application = () => {
                                 setPaymentBy(nextValue);
                             }}>
                             <Radio value="1" my={1}>
-                                Հաճախորդի կողմից
-                            </Radio>
-                            <Radio value="2" my={1}>
                                 Իմ կողմից
                             </Radio>
+                            <Radio value="2" my={1}>
+                                Հաճախորդի կողմից
+                            </Radio>
                         </Radio.Group>
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
                     </Stack>
+                    <Divider my={2} />
                     <Stack mx="0">
-                        <FormControl.Label>Ապրանքի արժեքը պահանջել հաճախորդից</FormControl.Label>
-                        <HStack><Text>Ոչ</Text><Switch /><Text>Այո</Text></HStack>
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
+                        <Checkbox
+                            value={customerShouldPayForProduct}
+                            onChange={handleCustomerShouldPayForProduct}
+                        >
+                            Ապրանքի արժեքը պահանջել հաճախորդից
+                        </Checkbox>
                     </Stack>
+
+                    {customerShouldPayForProduct && (
+                        <Box>
+                            <Divider my={2} />
+                            <Stack mx="0">
+                                <FormControl.Label>Ապրանքի արժեքը (դրամ)</FormControl.Label>
+                                <Input type="Text" defaultValue="" placeholder="" inputMode="numeric" />
+                            </Stack>
+                            {/* <Stack mx="0">
+                            <FormControl.Label>Ստանալ ապրանքի գումարը առաքիչից նախապես</FormControl.Label>
+                            <HStack><Text>Ոչ</Text><Switch /><Text>Այո</Text></HStack>
+                        </Stack> */}
+                        </Box>
+                    )}
                     <Stack mx="0">
-                        <FormControl.Label>Ապրանքի արժեքը (դրամ)</FormControl.Label>
-                        <Input type="Text" defaultValue="" placeholder="" inputMode="numeric" />
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
+                        <FormControl.Label>Այլ նշումներ</FormControl.Label>
+                        <TextArea />
                     </Stack>
-                    <Stack mx="0">
-                        <FormControl.Label>Ստանալ ապրանքի գումարը առաքիչից նախապես</FormControl.Label>
-                        <HStack><Text>Ոչ</Text><Switch /><Text>Այո</Text></HStack>
-                        {/* <FormControl.HelperText>
-                            Լրացրեք առաքվող ապրանքի նկարագիրը
-                        </FormControl.HelperText>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Atleast 6 characters are required.
-                        </FormControl.ErrorMessage> */}
-                    </Stack>
-                </FormControl>
-            </Box>
+                    <Button
+                        my={5}
+                        height={42}
+                        onPress={onSubmit}>Գրանցել առաքման պատվեր</Button>
+                </Box>
+            </ScrollView>
         </Box>
     );
 }
 
-export default Application;
+export default Application
